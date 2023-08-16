223a65f6cffcdd99fcac5f5d231b04a5128749be
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-08-16 18:17:43 +0200

    5

--- Controllers/Page2.cs
+++ Controllers/Page2.cs
@@ -7,9 +7,24 @@ public class Page2 : ControllerBase
 {
     [HttpGet]
     [Route("/{id}")]
-    public string Get([FromRoute] int id, [FromHeader] string someValue)
-
-{
-        return $"ID is {id} " + someValue;
+    public string GetId([FromRoute] int id)
+    {
+        return $"ID is {id} ";
+    }
+    
+    [HttpGet]
+    [Route("/readQuery")]
+    public string GetQuery([FromQuery] string someValue)
+    {
+        return $"ID is {someValue}";
+    }
+    
+    [HttpGet]
+    [Route("/readHeader")]
+    public string Get([FromHeader] string someValue)
+    {
+        
+        Console.WriteLine($"Received header value: {someValue}");
+        return $"check the http request headers for: {someValue}";
     }
 }
